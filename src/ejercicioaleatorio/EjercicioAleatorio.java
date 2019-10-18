abd002e96b11548e2e7fc8371fc8a8598a98e3d7
Usuario DAM 1 <Usuario DAM 1@DESKTOP-R6MG806>
2019-10-18 10:28:56 +0200

    todos los cambios estan bien.

---
+++ src/ejercicioaleatorio/EjercicioAleatorio.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioaleatorio;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario DAM 1
+ */
+public class EjercicioAleatorio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        int n1;
+        String contestacion;
+        int contador = 0;
+        int numAleatorio = 0;
+        do {
+                numAleatorio=(int) (Math.random() * 20);
+            do {
+                System.out.println("Adivina el numero de 0 a 20");
+                n1 = teclado.nextInt();
+                contador++;
+                if (n1 != numAleatorio) {
+                    System.out.println("Lo siento no has acertado");
+                }
+
+            } while (n1 != numAleatorio);
+            System.out.println("Muy bine has acertado el numero en " + contador + " intentos");
+            System.out.println("Quieres volver a jugar? S o N");
+
+            contestacion = teclado.next();
+        } while (contestacion.toUpperCase().equals("S"));
+        System.out.println("Has salido del juego");
+
+    }
+
+}
